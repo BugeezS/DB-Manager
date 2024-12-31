a5eaf45fb4187287dbab332ecc6766ba24a5b47c
Bugeez <brunolb.lafont@gmail.com>
2024-12-31 08:43:10 +0100

    add list all query api call

--- backend/src/databases/databases.module.ts
+++ backend/src/databases/databases.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabasesController } from './databases.controller';
 import { DatabasesService } from './databases.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [DatabasesController],
-  providers: [DatabasesService]
+  providers: [DatabasesService, PrismaService],
 })
 export class DatabasesModule {}
--- backend/src/databases/databases.service.ts
+++ backend/src/databases/databases.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class DatabasesService {
+  constructor(private prisma: PrismaService) {}
+
   async listDatabases(): Promise<{ databases: string[] }> {
-    return {
-      databases: ['database1', 'database2', 'database3'],
-    };
+    const databases = await this.prisma.database.findMany();
+    return { databases: databases.map((db) => db.name) };
   }
 }
